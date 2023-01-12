d5d3ea45f292f38b5583165bbbc6919d341c1e1a
Sarmad Wahab <sarmad@Sarmads-Laptop.local>
2023-01-12 15:08:32 -0600

    Test

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import { Box, Center } from "@chakra-ui/react";
+import FilePicker from "chakra-ui-file-picker";
 import Header from '../comps/header';
 
 function Index() {
     return (
         <Center>
             <Box w='95vw'>
-                <Header />
+                <FilePicker></FilePicker>
             </Box>
         </Center>
     );
